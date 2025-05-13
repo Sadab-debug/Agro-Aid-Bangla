c268ea0ca087de9112f0468660b9682e018077c2
Sadab <129375689+Sadab-debug@users.noreply.github.com>
2025-05-13 14:33:14 +0600

    new

--- app.py
+++ app.py
@@ -286,5 +286,5 @@ def chatbot():
 
 if __name__ == "__main__":
     os.makedirs('uploadimages', exist_ok=True)
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
+    port = int(os.environ.get("PORT", 4000))
     app.run(host="0.0.0.0", port=port)
